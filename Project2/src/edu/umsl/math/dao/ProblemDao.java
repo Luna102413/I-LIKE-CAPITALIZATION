333bf7e5044b05a70619dc7b234c921c07443a58
allison <allison@134.124.188.34>
2019-11-07 18:57:16 -0600

    Uploading Project1 files

---
+++ Project2/src/edu/umsl/math/dao/ProblemDao.java
@@ -0,0 +1,101 @@
+package edu.umsl.math.dao;
+
+import edu.umsl.math.beans.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.UnavailableException;
+
+public class ProblemDao {
+
+	private Connection connection;
+	private PreparedStatement results;
+	private PreparedStatement probcnt;
+
+	public ProblemDao() throws Exception {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://192.168.64.2:3306/mathprobdb", "admin", "");
+			
+			results = connection.prepareStatement(
+					"SELECT pid, content, order_num " + "FROM problem LIMIT ?, ?");
+			
+			probcnt = connection.prepareStatement("SELECT COUNT(pid) FROM problem");
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			throw new UnavailableException(exception.getMessage());
+		}
+
+	}
+	
+	public int getProblemCount() {
+		int cnt = 0;
+		
+		try {
+			ResultSet rs = probcnt.executeQuery();
+			rs.next();
+			
+			cnt = rs.getInt(1);
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		
+		return cnt;
+	}
+	
+	public List<Problem> getProblemList() throws SQLException {
+		List<Problem> problist = new ArrayList<Problem>();
+		
+		try {
+			ResultSet resultsRS = results.executeQuery();
+
+			while (resultsRS.next()) {
+				Problem prob = new Problem();
+
+				prob.setPid(resultsRS.getInt(1));
+				prob.setContent(resultsRS.getString(2));
+				prob.setOrder_num(resultsRS.getInt(3));
+
+				problist.add(prob);
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		
+		return problist;
+	}
+	
+	public List<Problem> getProblemListByPage(int pg) throws SQLException {
+		List<Problem> problist = new ArrayList<Problem>();
+		
+		int st = 10 * (pg - 1);
+		
+		try {
+			results.setInt(1, st);
+			results.setInt(2, 10);
+			
+			ResultSet resultsRS = results.executeQuery();
+
+			while (resultsRS.next()) {
+				Problem prob = new Problem();
+
+				prob.setPid(resultsRS.getInt(1));
+				prob.setContent(resultsRS.getString(2));
+				prob.setOrder_num(resultsRS.getInt(3));
+
+				problist.add(prob);
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		
+		return problist;
+	}
+
+}
